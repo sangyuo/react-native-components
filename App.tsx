aae9fbddda038d6f4da195fb182e46cd7b5bbed4
sangyuo <sangxilam@gmail.com>
2024-10-22 16:56:25 +0700

    fix: error app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, useColorScheme} from 'react-native';
+import {SafeAreaView, ScrollView} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {Box, Button, ProgressBar, ProgressCircle} from './src/components';
+import {Box, ButtonBox, ProgressBar, ProgressCircle} from './src/components';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
   const [value, setValue] = React.useState(0);
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <ScrollView>
         <Box className="bg-secondary-light mb-5">
-          <Button title="Progress up to 100" onPress={() => setValue(100)} />
+          <ButtonBox title="Progress up to 100" onPress={() => setValue(100)} />
         </Box>
         <Box className="bg-secondary-light h-10 mb-5">
-          <Button title="Progress up to 25" onPress={() => setValue(25)} />
+          <ButtonBox title="Progress up to 25" onPress={() => setValue(25)} />
         </Box>
         <Box className="gap-2">
           <ProgressBar value={value} varian="secondary" />
