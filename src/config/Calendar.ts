876b8d50bb5e560d203cce39dfdabaf7e67b14fa
sangyuo <sangxilam@gmail.com>
2024-12-18 18:01:14 +0700

    feat: Custom styles calendar

---
+++ src/config/Calendar.ts
@@ -0,0 +1,51 @@
+const WEEKS_SHORT = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const MONTHS_SHORT = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+const MONTHS_LONG = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const WEEKS_LONG = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+export const CALENDAR = {
+  week: {
+    short: WEEKS_SHORT,
+    long: WEEKS_LONG,
+  },
+  month: {
+    short: MONTHS_SHORT,
+    long: MONTHS_LONG,
+  },
+};
